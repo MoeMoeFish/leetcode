e9ad0e4918148bd3520883739fc7797468dec267
MoeMoeFish <moemoefish@qq.com>
2020-06-29 13:45:14 +0800

    doing 0002

--- src/0002-AddTwoNumbers/main.js
+++ src/0002-AddTwoNumbers/main.js
@@ -1,3 +1,5 @@
+const ListNode = require('./common').ListNode;
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -5,12 +7,37 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
+  const r = new ListNode();
+  let l = r;
+  let carry = 0;
+  while (l1 && l2) {
+    let and = l1.val + l2.val + carry;
+    if (and >= 10) {
+      and = and - 10;
+      carry = 1;
+    }
+    const node = new ListNode(and);
+    l.next = node;
+
+    l = l.next;
+    l1 = l1.next;
+    l2 = l2.next;
+  }
+
+  const left = l1 != null ? l1 : l2 != null ? l2 : null;
+  if (left) {
+    left.val += carry;
+    r.next = left;
+  }
+
+  return r.next;
 };
 
 module.exports = addTwoNumbers;
--- src/0002-AddTwoNumbers/main.test.js
+++ src/0002-AddTwoNumbers/main.test.js
@@ -1,5 +1,7 @@
 const algo = require('./main');
+const toList = require('./common').toList;
+const toArray = require('./common').toArray;
 
 it('2. Add Two Numbers', () => {
-  expect(algo([2, 4, 3], [5, 6, 4])).toEqual([7, 0, 8]);
+  expect(toArray(algo(toList([2, 4, 3]), toList([5, 6, 4])))).toEqual([7, 0, 8]);
 });
