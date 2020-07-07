238c7deca1bf1ad90ecab674feb503d5a36726a4
MoeMoeFish <moemoefish@qq.com>
2020-07-07 13:20:52 +0800

    0002 finished

--- src/0002-AddTwoNumbers/main.js
+++ src/0002-AddTwoNumbers/main.js
@@ -17,24 +17,22 @@ var addTwoNumbers = function(l1, l2) {
   const r = new ListNode();
   let l = r;
   let carry = 0;
-  while (l1 && l2) {
-    let and = l1.val + l2.val + carry;
+  while (l1 || l2 || carry) {
+    const n1 = l1 ? l1.val : 0;
+    const n2 = l2 ? l2.val : 0;
+    let and = n1 + n2 + carry;
     if (and >= 10) {
       and = and - 10;
       carry = 1;
+    } else {
+      carry = 0;
     }
     const node = new ListNode(and);
     l.next = node;
 
     l = l.next;
-    l1 = l1.next;
-    l2 = l2.next;
-  }
-
-  const left = l1 != null ? l1 : l2 != null ? l2 : null;
-  if (left) {
-    left.val += carry;
-    r.next = left;
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
   }
 
   return r.next;
